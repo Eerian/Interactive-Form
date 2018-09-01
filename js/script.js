c7393bf409434bb30c3d46422fd527958a2166f2
Erion Pali <eerian00@gmail.com>
2018-09-01 17:10:06 -0400

    Added functionality to Activities Section
    * When selecting checkboxes a total cost is calculated at the bottom
    
    * When an activity conflics with another activity's time. The latter gets disabled.

--- js/script.js
+++ js/script.js
@@ -55,17 +55,102 @@ for (let i = 0; i < designs.length; i++) {
 }
 
 
-// const allActivities = document.querySelectorAll('.activities input');
-// console.log(allActivities);
+const allActivities = document.querySelectorAll('.activities input');
+const activitiesDiv = document.querySelector('.activities');
+//create a div to store total cost of activities
+let totalDiv = document.createElement('div');
 
-// // allActivities.addEventListener('change', (event) => {
-// //     const isChecked = event.target.checked;
-// //     if (isChecked) {
-// //         for (let i = 0; i < allActivities.length; i++) {
+//keeps tract of activities costs
+let totalCost = 0;
 
-// //         }
-// //     } else {
+//loop thru all activities inputs and add event listeners to all
+for (let i = 0; i < allActivities.length; i++) {
+    allActivities[i].addEventListener('change', (event) => {
+        //create a isChecked variable to check it against activity checkboxes
+        const isChecked = event.target.checked;
+        //if the first checkbox is clicked and isChecked add 200 to cost.
+        if (event.target == allActivities[0] && isChecked) {
+            totalCost += 200;
+            //if the first one is unchecked remove 200 from cost
+        } else if (event.target == allActivities[0] && !isChecked) {
+            totalCost -= 200;
+            //if the second checkbox is clicked and isChecked add 100 to cost
+        } else if (event.target == allActivities[1] && isChecked) {
+            totalCost += 100;
+            //disable the 4th checkbox because of time conflict and turn it gray
+            allActivities[3].disabled = true;
+            allActivities[3].parentNode.style.color = "gray";
+            //if the second checkbox is unchecked enable the 4th checkbox and remove 100 from cost.
+        } else if (event.target == allActivities[1] && !isChecked) {
+            totalCost -= 100;
+            allActivities[3].disabled = false;
+            allActivities[3].parentNode.style.color = "";
+            //THE REST OF THE STATEMENTS FOLLOW THE SAME IDEA AS ABOVE.    
+        } else if (event.target == allActivities[2] && isChecked) {
+            totalCost += 100;
+            allActivities[4].disabled = true;
+            allActivities[4].parentNode.style.color = "gray";
+        } else if (event.target == allActivities[2] && !isChecked) {
+            totalCost -= 100;
+            allActivities[4].disabled = false;
+            allActivities[4].parentNode.style.color = "";
+        } else if (event.target == allActivities[3] && isChecked) {
+            totalCost += 100;
+            allActivities[1].disabled = true;
+            allActivities[1].parentNode.style.color = "gray";
+        } else if (event.target == allActivities[3] && !isChecked) {
+            totalCost -= 100;
+            allActivities[1].disabled = false;
+            allActivities[1].parentNode.style.color = "";
+        } else if (event.target == allActivities[4] && isChecked) {
+            totalCost += 100;
+            allActivities[2].disabled = true;
+            allActivities[2].parentNode.style.color = "gray";
+        } else if (event.target == allActivities[4] && !isChecked) {
+            totalCost -= 100;
+            allActivities[2].disabled = false;
+            allActivities[2].parentNode.style.color = "";
+        } else if (event.target == allActivities[5] && isChecked) {
+            totalCost += 100;
+        } else if (event.target == allActivities[5] && !isChecked) {
+            totalCost -= 100;
+        } else if (event.target == allActivities[6] && isChecked) {
+            totalCost += 100;
+        } else if (event.target == allActivities[6] && !isChecked) {
+            totalCost -= 100;
+        }
+        //update total cost based on selections above
+        totalDiv.textContent = "Total: " + totalCost;
+        //append the totalCost to the DIV created at the bottom of the activities list.
+        activitiesDiv.appendChild(totalDiv);
+    });
 
-// //     }
+}
 
-// // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const mainConference = document.querySelector('input[name="all"]');
+// const jsFrameworks = document.querySelector('input[name="js-frameworks"]');
+// const jsLibraries = document.querySelector('input[name="js-libs"]');
+// const expressWorkshop = document.querySelector('input[name="express"]');
+// const nodeWorkshop = document.querySelector('input[name="node"]');
+// const buildTools = document.querySelector('input[name="build-tools"]');
+// const npmWorkshop = document.querySelector('input[name="npm"]');
