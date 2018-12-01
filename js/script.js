1c81757afb16fc843d387016fea0fe798168afaa
Erion Pali <eerian00@gmail.com>
2018-12-01 05:20:40 -0500

    Added validations
    1-Added validations for  Credit Card
    2-Added validation for Name
    3-Added Validation for Email
    4-Added validation for Activities
    
    5-Design selections now show all colors when no theme is selected
    
    6-Added styles to Error messages

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const nameField = document.querySelector('#name');
 nameField.focus();
 
 
+
 /***JOB ROLE SECTION ******/
 
 //hide input field when the option "other" is selected in job role.
@@ -54,6 +55,10 @@ for (let i = 0; i < designs.length; i++) {
                 allColorsParentDiv.removeChild(jsPunsThemeColors[i]);
                 allColorsParentDiv.appendChild(iLoveJSThemeColors[i]);
             }
+            //if no theme is selected display all colors
+        } else if (event.target.value == 'Select Theme') {
+            allColorsParentDiv.appendChild(iLoveJSThemeColors[i]);
+            allColorsParentDiv.appendChild(jsPunsThemeColors[i]);
         }
     });
 }
@@ -65,10 +70,10 @@ const activitiesDiv = document.querySelector('.activities');
 //create a div to store total cost of activities
 let totalDiv = document.createElement('div');
 
-//keeps tract of activities costs
+// Keeps tract of activities costs
 let totalCost = 0;
 
-//loop thru all activities inputs and add event listeners to all
+// loop thru all activities inputs and add event listeners to all
 for (let i = 0; i < allActivities.length; i++) {
     allActivities[i].addEventListener('change', (event) => {
         //create a isChecked variable to check it against activity checkboxes
@@ -134,24 +139,25 @@ for (let i = 0; i < allActivities.length; i++) {
 
 /***PAYMENT INFO SECTION **************************************************************************/
 
-const allPayments = document.querySelectorAll('#payment');
-const paymentSection = document.querySelector('#payment');
-const paymentInfos = document.querySelectorAll('div p');
+const allPaymentOptions = document.querySelector('#payment');
 const creditCard = document.querySelector('#credit-card');
+//select paypal and bitcoin divs
+const paypalAndBitcoin = document.querySelectorAll('div p');
+const paypal = paypalAndBitcoin[0];
+const bitcoin = paypalAndBitcoin[1];
 
-const paypal = paymentInfos[0];
-const bitcoin = paymentInfos[1];
+//functions for hiding and showing payment options
 const hide = (payment) => payment.style.display = 'none';
 const show = (payment) => payment.style.display = 'block';
 
 
-//make credit card as default payment selection
-paymentSection.selectedIndex = '1';
+// Make credit card as default payment selection
+allPaymentOptions.selectedIndex = '1';
 hide(paypal);
 hide(bitcoin);
 
-for (let i = 0; i < allPayments.length; i++) {
-    allPayments[i].addEventListener('change', (event) => {
+for (let i = 0; i < allPaymentOptions.length; i++) {
+    allPaymentOptions.addEventListener('change', (event) => {
         if (event.target.value == 'credit card') {
             hide(paypal);
             hide(bitcoin);
@@ -173,63 +179,172 @@ for (let i = 0; i < allPayments.length; i++) {
 
 const submitButton = document.querySelector('button');
 const email = document.querySelector('#mail');
-const container = document.querySelectorAll('fieldset')[0];
-//select email label to insert error message for "name" before it.
+//select all children within the Basic Info section
+const basicInfoFieldset = document.querySelectorAll('fieldset')[0];
+//select all children within the Activities Section
+const activitiesFieldset = document.querySelectorAll('fieldset')[2];
+// Select email label to insert error message for "name" before it.
+//also used to turn email red if needed
 const emailLabel = document.querySelectorAll('fieldset :nth-child(4)')[0];
+//Select Job Role label to insert error message for "email" before it.
+const jobRoleLabel = document.querySelectorAll('fieldset :nth-child(6)')[0];
+//select Register for Activities title to insert error message for activities after it.
+const regForActivities = document.querySelector('.activities :nth-child(2)');
 
 
 
-//Validate name
+
+const styleError = (p) => {
+    //styles for the error message.
+    p.style.marginTop = "-10px";
+    p.style.paddingBottom = "10px";
+    p.style.paddingTop = "5px";
+    p.style.background = "pink";
+    p.style.color = "red";
+}
+
+const makeRed = (label) => {
+    label.style.color = 'red';
+}
+
+//*** */ Validate name
+//select Name label to turn red if error
+const nameLabel = document.querySelector('label[for=name]');
+
 const checkName = (name) => {
     const nameInput = /^[A-Za-z//]+$/;
     if (nameInput.test(name) == false) {
+        //create an error paragraph
         const p = document.createElement('p');
+        p.innerText = 'Name must not be left empty and must contain only letters!';
         //styles for the error message.
-        p.style.marginTop = "-15px";
-        p.style.paddingBottom = "10px";
-        p.style.background = "pink";
-        p.style.color = "red";
-
-        p.innerText = 'Name must not be left empty and must only contain letters.';
+        //make name and error message red
+        makeRed(nameLabel);
+        styleError(p);
         //insert error message right on top of email: so it shows under Name:
-        container.insertBefore(p, emailLabel);
-        console.log("Error");
+        basicInfoFieldset.insertBefore(p, emailLabel);
     }
 }
 
-// //Validate Email
-// const checkEmail = (email) => {
-//     const emailInput = /\w+/;
-//     if (emailInput.test(email) == false) {
-//        alert('Please enter a valid email');
-//     }   
-// }
+//*** */ Validate Email
+//select email label to turn red in case of error
 
-submitButton.addEventListener('click', () => {
+const checkEmail = (email) => {
+    //regex to accept (letters, numbers, . and _ symbols before @ sign)
+    //and to accept (letters, numbers, . and _ symbols after @ sign)
+    //then accept (.) then after the (.) accept letters only from 2-4 letters long.
+    const emailInput = /^[a-zA-Z0-9._]+@[a-zA-Z0-9._]+\.[a-zA-Z]{2,4}$/;
+    if (emailInput.test(email) == false) {
+        //create an error paragraph
+        const p = document.createElement('p');
+        p.innerText = 'Email must be in a "name@email.com" format!';
+
+        //styles for the error message
+        makeRed(emailLabel);
+        styleError(p);
+
+        //insert error message right on top of Job Role: so it shows under Email:
+        basicInfoFieldset.insertBefore(p, jobRoleLabel);
+    }
+}
+
+//*** /Validare if at least one checkBox is checked.
+
+//select Register for Activities label to turn red in case of error.
+const regForActivitiesLabel = document.querySelector('.activities legend');
+
+const validateCheckBox = () => {
+    for (let i = 0; i < allActivities.length; i++) {
+        if (allActivities[i].checked) {
+            return true;
+        }
+    }
+    //create error message
+    const p = document.createElement('p');
+    p.innerText = 'Select at least one activity';
+    //styles for the error message.
+    makeRed(regForActivitiesLabel);
+    styleError(p);
+
+    //insert error message below Register For Activities
+    activitiesFieldset.insertBefore(p, regForActivities);
+}
+
+
+
+
+//*** */Validate Credit Card
+const creditCardNumber = document.querySelector('#cc-num');
+const zipcode = document.querySelector('#zip');
+const securityCode = document.querySelector('#cvv');
+
+//Select labels to turn red in case of error
+const cardNumberLabel = document.querySelector('label[for=cc-num]');
+const zipCodeLabel = document.querySelector('label[for=zip]');
+const cvvLabel = document.querySelector('label[for=cvv]');
+const cardNumberDiv = document.querySelector('#credit-card div');
+
+
+const checkCreditCard = (card, zip, cvv) => {
+    //Accept CC numbers 13 to 16 digits long
+    const creditCardInput = /^\d{13,16}$/;
+    if (creditCardInput.test(card) == false) {
+        //create error message
+        const p = document.createElement('p');
+        p.innerText = 'Please Enter Credit Card number 13-16 digits long.';
+
+        //styles for error message
+        makeRed(cardNumberLabel);
+        styleError(p);
+
+        //insert error message on top of Card Number
+        creditCard.insertBefore(p, cardNumberDiv);
+
+    }
+    //Accept Zipcode 5 digits long
+    const zipcodeInput = /^\d{5}$/;
+    if (zipcodeInput.test(zip) == false) {
+        //create error message
+        const p = document.createElement('p');
+        p.innerText = 'Zip Code must be 5 digits';
+
+        //styles for error message
+        makeRed(zipCodeLabel);
+        styleError(p);
+
+        //insert error on top of Zip Code
+        creditCard.insertBefore(p, cardNumberDiv);
+    }
+
+    //Accept security code 3 digits long
+    const securityCodeInput = /^\d{3}$/;
+    if (securityCodeInput.test(cvv) == false) {
+        //create error message
+        const p = document.createElement('p');
+        p.innerText = 'CVV Code must be 3 digits';
+
+        //styles for error message
+        makeRed(cvvLabel);
+        styleError(p);
+
+        //insert error on top of cvv Code
+        creditCard.insertBefore(p, cardNumberDiv);
+    }
+
+}
+
+
+
+// Add eventListener to Submit Button
+submitButton.addEventListener('click', (e) => {
+
+    e.preventDefault();
+
     checkName(nameField.value);
     // checkEmail(email.value);
+    // validateCheckBox();
+    // if (allPaymentOptions.value == 'credit card') {
+    //     checkCreditCard(creditCardNumber.value, zipcode.value, securityCode.value);
+    // }
+
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const mainConference = document.querySelector('input[name="all"]');
-// const jsFrameworks = document.querySelector('input[name="js-frameworks"]');
-// const jsLibraries = document.querySelector('input[name="js-libs"]');
-// const expressWorkshop = document.querySelector('input[name="express"]');
-// const nodeWorkshop = document.querySelector('input[name="node"]');
-// const buildTools = document.querySelector('input[name="build-tools"]');
-// const npmWorkshop = document.querySelector('input[name="npm"]');
